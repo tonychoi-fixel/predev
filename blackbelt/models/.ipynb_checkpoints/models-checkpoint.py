403cfa77e3b9e5b1bb0b1f85b1c4291c6e029ba2
Tony Choi <tony.choi@fixel.us>
2020-12-29 19:00:30 +0000

    Add existing file

--- blackbelt/models/.ipynb_checkpoints/models-checkpoint.py
+++ blackbelt/models/.ipynb_checkpoints/models-checkpoint.py
@@ -7,10 +7,45 @@ class blackbelt(models.Model):
      _name = 'blackbelt.blackbelt'
      _description = 'blackbelt.blackbelt'
 
-     name = fields.Char()
+     name = fields.Char(string = 'Device Id')
+     serialnumber = fields.Char(string = 'Serial Number')
+     manufacturer = fields.Char()
+     model = fields.Char()
+     modelnumber = fields.Char()
+     storage = fields.Char()
+     color = fields.Char()
+     carrier = fields.Char()
+     imei = fields.Char()
+     imei2 = fields.Char()
+     wipe_type = fields.Char()
+     wipe_method = fields.Char()
+     wipe_result = fields.Char()
+     wipe_date = fields.Datetime(string='Lastest Wipe Date')
+     blacklisted = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+     date_create = fields.Datetime(string='Create Date', required=True, readonly=True, index=True, copy=False, default=fields.Datetime.now, help="Creation date of Serial number by blackbelt")
+     
+
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+            
+class blackbeltserial(models.Model):
+     _name = 'blackbelt.serial'
+     _description = 'blackbelt.serial'
+
+     name = fields.Char(string = 'Serial Number')
+     manufacturer = fields.Char()
+     date_create = fields.Datetime(string='Create Date', required=True, readonly=True, index=True, copy=False, default=fields.Datetime.now, help="Creation date of Serial number by blackbelt")
+    
+    
      value = fields.Integer()
      value2 = fields.Float(compute="_value_pc", store=True)
      description = fields.Text()
+     
 
      @api.depends('value')
      def _value_pc(self):
